28def618ac28fe4b5c322d144d9c833517ff681c
MarcosNBJ <marcosnery.comp@gmail.com>
2020-11-13 18:52:29 -0300

    Working request

--- web/app.py
+++ web/app.py
@@ -9,18 +9,27 @@ from flask import request
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 @app.route('/')
 def index():
     genres = ["Action","Comedy","Drama","Fantasy","Horror","Mystery","Romance","Thriller","Western"]
     return render_template('index.html', genres=genres)
 
-@app.route('/match', methods=['POST'])
+@app.route('/match', methods=['GET','POST'])
 def match():
     user_order = request.json['user_order']
-    return ' '.join(user_order)
-    #return render_template('match.html', user_order=user_order)
+    return render_template('match.html', user_order=user_order)
 
 
 if __name__ == '__main__':
--- web/static/js/script.js
+++ web/static/js/script.js
@@ -4,27 +4,35 @@ $(function() {
 
     sortable('.js-sortable', {
             forcePlaceholderSize: true,
-            placeholderClass: 'mb1 litemdark ',
+            placeholderClass: 'mb1 bg-navy',
             hoverClass: 'bg-maroon yellow'
         })
+
+
         
-    // sortable('.sortable')[0].addEventListener('sortupdate', function(e) {
-    //         e.detail.origin.items.forEach(element => {user_order.push(element.id)});
-    // });
+    sortable('.sortable')[0].addEventListener('sortupdate', function(e) {
+            e.detail.origin.items.forEach(element => {user_order.push(element.id)});
+    });
 
 
-    // $("#button").click(function(){
+    $("#button").click(function(){
+        console.log("bb");
 
-    //     $.ajax({
-    //         url: "/match",
-    //         type: "post",
-    //         data: {"user_order": user_order},
-    //         success: function(response) {
-    //             console.log(response);
-    //             // $("#result").html(response);
-    //         }
-    //     });
-    // });
+        $.ajax({
+            url: "/match",
+            type: "POST",
+            // dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                "user_order":"Test Name"
+              }),
+            success: function(response) {
+                $('#result').append(response)
+            }
+        });
+
+
+    });
 
 
 
